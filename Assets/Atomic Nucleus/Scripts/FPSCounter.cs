5a55a54a230ce9e833bc47cb1c52ecdfd2dac446
Miguel Fontoura <miguel.fontoura1@gmail.com>
2017-09-04 20:33:27 +0100

    WIP: Nucleus project

---
+++ Assets/Atomic Nucleus/Scripts/FPSCounter.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class FPSCounter : MonoBehaviour {
+
+
+    public int AverageFPS{ get; private set; }
+    public int HighestFPS{ get; private set; }
+    public int LowestFPS{ get; private set; }
+    public int frameRange = 60;
+
+    private int[] fpsBuffer;
+    private int fpsBufferIndex;
+
+    private void Update(){
+        if (fpsBuffer == null || fpsBuffer.Length != frameRange){
+            Initialize();
+        }
+        UpdateBuffer();
+        CalculateFPS();
+    }
+
+    private void Initialize(){
+        if (frameRange <= 0){
+            frameRange = 1;
+        }
+
+        fpsBuffer = new int[frameRange];
+        fpsBufferIndex = 0;
+    }
+
+    private void UpdateBuffer(){
+        fpsBuffer[fpsBufferIndex++] = (int) (1f / Time.unscaledDeltaTime);
+        if (fpsBufferIndex >= frameRange){
+            fpsBufferIndex = 0;
+        }
+    }
+
+    private void CalculateFPS(){
+        int sum = 0;
+        int highest = 0;
+        int lowest = int.MaxValue;
+        for (int i = 0; i < frameRange; i++){
+            int fps = fpsBuffer[i];
+            sum += fps;
+            if (fps > highest){
+                highest = fps;
+            }
+            if (fps < lowest){
+                lowest = fps;
+            }
+        }
+
+        AverageFPS = sum / frameRange;
+        HighestFPS = highest;
+        LowestFPS = lowest;
+
+
+    }
+}
