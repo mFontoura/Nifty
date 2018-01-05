480d5dfba45129e9f755ec1ad1bdb942033e0b4d
Miguel Fontoura <miguel.fontoura1@gmail.com>
2018-01-05 00:07:44 +0000

    Too much math for me... will update on another day

--- Assets/Curves And Splines/Editor/BezierCurveInspector.cs
+++ Assets/Curves And Splines/Editor/BezierCurveInspector.cs
@@ -11,6 +11,8 @@ public class BezierCurveInspector : Editor
     private Quaternion _handleRotation;
 
     private const int LineSteps = 10;
+    
+    private const float directionScale = 0.5f;
 
     private void OnSceneGUI(){
         _curve = target as BezierCurve;
@@ -20,19 +22,25 @@ public class BezierCurveInspector : Editor
         Vector3 p0 = ShowPoint(0);
         Vector3 p1 = ShowPoint(1);
         Vector3 p2 = ShowPoint(2);
+        Vector3 p3 = ShowPoint(3);
 
         Handles.color = Color.gray;
         Handles.DrawLine(p0, p1);
-        Handles.DrawLine(p1,p2);
+        Handles.DrawLine(p2,p3);
         
-        Handles.color = Color.white;
-        Vector3 lineStart = _curve.GetPoint(0f);
-        for (int i = 1; i <= LineSteps; i++){
-            Vector3 lineEnd = _curve.GetPoint(i / (float) LineSteps);
-            Handles.DrawLine(lineStart, lineEnd);
-            lineStart = lineEnd;
-        }
+        ShowDirections();
+        Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
 
+    }
+    
+    private void ShowDirections () {
+        Handles.color = Color.green;
+        Vector3 point = _curve.GetPoint(0f);
+        Handles.DrawLine(point, point + _curve.GetDirection(0f) * directionScale);
+        for (int i = 1; i <= LineSteps; i++) {
+            point = _curve.GetPoint(i / (float)LineSteps);
+            Handles.DrawLine(point, point + _curve.GetDirection(i / (float)LineSteps) * directionScale);
+        }
     }
 
     private Vector3 ShowPoint(int index){
