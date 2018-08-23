23da7c13abe387f2b5ade5995da36b29222d0f34
Miguel Fontoura <miguelfontoura@collide.rocks>
2018-08-23 22:53:46 +0100

    WIP: created as cube

---
+++ Assets/ProceduralGrid/Cube.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Cube : MonoBehaviour
+{
+
+	[SerializeField]
+	private int _xSize, _ySize, _zSize;
+
+	private Vector3[] _vertices;
+	private Mesh _mesh;
+
+	private void Awake(){
+		Generate();
+	}
+
+	private void Generate(){
+        
+		GetComponent<MeshFilter>().mesh = _mesh = new Mesh();
+		_mesh.name = "Procedural Cube";
+
+		CreateVertices();
+		CreateTriangles();
+        
+	}
+
+	private void CreateTriangles()
+	{
+		int quads = (_xSize * _ySize + _xSize * _zSize + _ySize * _zSize) * 2;
+		int[] triangles = new int[quads * 6];
+		int ring = (_xSize + _zSize) * 2;
+		int t = 0, v = 0;
+		for (int y = 0; y < _ySize; y++, v++) {
+			for (int q = 0; q < ring - 1; q++, v++) {
+				t = SetQuad(triangles, t, v, v + 1, v + ring, v + ring + 1);
+			}
+
+			t = SetQuad(triangles, t, v, v - ring + 1, v + ring, v + 1);
+		}
+		t = CreateTopFace(triangles, t, ring);
+		t = CreateBottomFace(triangles, t, ring);
+		_mesh.triangles = triangles;
+	}
+
+	private void CreateVertices()
+	{
+		int cornerVertices = 8;
+		int edgeVertices = (_xSize + _ySize + _zSize - 3) * 4;
+		int faceVertieces = (
+			                    (_xSize - 1) * (_ySize - 1) +
+			                    (_xSize - 1) * (_zSize - 1) +
+			                    (_ySize - 1) * (_zSize - 1)) * 2;
+		_vertices = new Vector3[cornerVertices + edgeVertices + faceVertieces];
+
+		int v = 0;
+		for (int y = 0; y <= _ySize; y++) {
+			for (int x = 0; x <= _xSize; x++) {
+				_vertices[v++] = new Vector3(x, y, 0);
+			}
+
+			for (int z = 1; z <= _zSize; z++) {
+				_vertices[v++] = new Vector3(_xSize, y, z);
+			}
+
+			for (int x = _xSize - 1; x >= 0; x--) {
+				_vertices[v++] = new Vector3(x, y, _zSize);
+			}
+
+			for (int z = _zSize - 1; z > 0; z--) {
+				_vertices[v++] = new Vector3(0, y, z);
+			}
+		}
+
+		for (int z = 1; z < _zSize; z++) {
+			for (int x = 1; x < _xSize; x++) {
+				_vertices[v++] = new Vector3(x, _ySize, z);
+			}
+		}
+
+		for (int z = 1; z < _zSize; z++) {
+			for (int x = 1; x < _xSize; x++) {
+				_vertices[v++] = new Vector3(x, 0, z);
+			}
+		}
+
+		_mesh.vertices = _vertices;
+	}
+	
+	private int CreateTopFace (int[] triangles, int t, int ring) {
+		int v = ring * _ySize;
+		for (int x = 0; x < _xSize - 1; x++, v++) {
+			t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + ring);
+		}
+		t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + 2);
+		
+		int vMin = ring * (_ySize + 1) - 1;
+		int vMid = vMin + 1;
+		int vMax = v + 2;
+
+		for (int z = 1; z < _zSize - 1; z++, vMin--, vMid++, vMax++) {
+			t = SetQuad(triangles, t, vMin, vMid, vMin - 1, vMid + _xSize - 1);
+			for (int x = 1; x < _xSize - 1; x++, vMid++) {
+				t = SetQuad(
+					triangles, t,
+					vMid, vMid + 1, vMid + _xSize - 1, vMid + _xSize);
+			}
+
+			t = SetQuad(triangles, t, vMid, vMax, vMid + _xSize - 1, vMax + 1);
+		}
+
+		int vTop = vMin - 2;
+			t = SetQuad(triangles, t, vMin, vMid, vTop + 1, vTop);
+			for (int x = 1; x < _xSize - 1; x++, vTop--, vMid++) {
+				t = SetQuad(triangles, t, vMid, vMid + 1, vTop, vTop - 1);
+			}
+			t = SetQuad(triangles, t, vMid, vTop - 2, vTop, vTop - 1);
+
+		return t;
+	}
+	
+	private int CreateBottomFace (int[] triangles, int t, int ring) {
+		int v = 1;
+		int vMid = _vertices.Length - (_xSize - 1) * (_zSize - 1);
+		t = SetQuad(triangles, t, ring - 1, vMid, 0, 1);
+		for (int x = 1; x < _xSize - 1; x++, v++, vMid++) {
+			t = SetQuad(triangles, t, vMid, vMid + 1, v, v + 1);
+		}
+		t = SetQuad(triangles, t, vMid, v + 2, v, v + 1);
+
+		int vMin = ring - 2;
+		vMid -= _xSize - 2;
+		int vMax = v + 2;
+
+		for (int z = 1; z < _zSize - 1; z++, vMin--, vMid++, vMax++) {
+			t = SetQuad(triangles, t, vMin, vMid + _xSize - 1, vMin + 1, vMid);
+			for (int x = 1; x < _xSize - 1; x++, vMid++) {
+				t = SetQuad(
+					triangles, t,
+					vMid + _xSize - 1, vMid + _xSize, vMid, vMid + 1);
+			}
+			t = SetQuad(triangles, t, vMid + _xSize - 1, vMax + 1, vMid, vMax);
+		}
+
+		int vTop = vMin - 1;
+		t = SetQuad(triangles, t, vTop + 1, vTop, vTop + 2, vMid);
+		for (int x = 1; x < _xSize - 1; x++, vTop--, vMid++) {
+			t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vMid + 1);
+		}
+		t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vTop - 2);
+		
+		return t;
+	}
+	
+	private static int SetQuad (int[] triangles, int i, int v00, int v10, int v01, int v11) {
+		triangles[i] = v00;
+		triangles[i + 1] = triangles[i + 4] = v01;
+		triangles[i + 2] = triangles[i + 3] = v10;
+		triangles[i + 5] = v11;
+		return i + 6;
+	}
+
+	private void OnDrawGizmos(){
+        
+		//because it gives error if not on play mode
+		if (_vertices == null){
+			return;
+		}
+        
+		Gizmos.color = Color.black;
+		for (var i = 0; i < _vertices.Length; i++){
+			Gizmos.DrawSphere(_vertices[i], 0.1f);
+		}
+	}
+}
--- Assets/ProceduralGrid/Grid.cs
+++ Assets/ProceduralGrid/Grid.cs
@@ -4,58 +4,57 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Grid : MonoBehaviour
 {
-    [SerializeField]
-    private int _xSize, _ySize;
+    [SerializeField] private int _xSize, _ySize;
 
     private Vector3[] _vertices;
     private Mesh _mesh;
 
-    private void Awake(){
+    private void Awake()
+    {
         StartCoroutine(Generate());
     }
 
-    private IEnumerator Generate(){
+    private IEnumerator Generate()
+    {
         WaitForSeconds wait = new WaitForSeconds(0.02f);
-        
+
         GetComponent<MeshFilter>().mesh = _mesh = new Mesh();
         _mesh.name = "Procedural Grid";
-        
-        _vertices = new Vector3[(_xSize+1)*( _ySize+1)];
+
+        _vertices = new Vector3[(_xSize + 1) * (_ySize + 1)];
         Vector2[] uv = new Vector2[_vertices.Length];
         Vector4[] tangents = new Vector4[_vertices.Length];
-        Vector4 tangent = new Vector4(1f,0f,0f,-1f);
-        for (int i = 0, y = 0; y <= _ySize; y++){
-            for (int x = 0; x <= _xSize; x++, i++){
-                _vertices[i] = new Vector3(x,y);
-                uv[i] = new Vector2((float)x/_xSize, (float)y/_ySize);
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+        for (int i = 0, y = 0; y <= _ySize; y++) {
+            for (int x = 0; x <= _xSize; x++, i++) {
+                _vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float) x / _xSize, (float) y / _ySize);
                 tangents[i] = tangent;
                 yield return wait;
             }
         }
-        
+
         _mesh.vertices = _vertices;
         _mesh.uv = uv;
         _mesh.tangents = tangents;
-        
+
         int[] triangles = new int[_xSize * _ySize * 6];
-        for (int ti = 0, vi = 0, y = 0; y < _ySize; y++, vi++){
-            for (int x = 0; x < _xSize; x++ ,ti += 6, vi++) {
+        for (int ti = 0, vi = 0, y = 0; y < _ySize; y++, vi++) {
+            for (int x = 0; x < _xSize; x++, ti += 6, vi++) {
                 triangles[ti] = vi;
                 triangles[ti + 3] = triangles[ti + 2] = vi + 1;
                 triangles[ti + 4] = triangles[ti + 1] = vi + _xSize + 1;
                 triangles[ti + 5] = vi + _xSize + 2;
                 _mesh.triangles = triangles;
-                
+
                 yield return wait;
             }
-            
-        }
-        
-        _mesh.RecalculateNormals();
-        
-        
-    }
 
+        }
+
+        _mesh.RecalculateNormals();
+    }
+    
     private void OnDrawGizmos(){
         
         //because it gives error if not on play mode
